4cf7bc4b9c61f8199289513cd3b31e197a5695af
S4Lsalsoft <grupo.fisica.638@gmail.com>
2020-03-16 10:10:54 -0430

    no message

---
+++ BarradeProgreso/BarradeProgreso/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(204, 71)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Real Progress bar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 120)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(414, 33)
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(222, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(204, 71)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Fake Progress bar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 171)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(414, 225)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(444, 421)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ BarradeProgreso/BarradeProgreso/Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class Form1
+
+    '/////////////////////////////////////////////////////////////////////////////////////////
+    'Real Progress Bar . (se muestra el Progreso Real)
+    '
+    'Se necesita :
+    '1 Progressbar.
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ProgressBar1.Value = 0 ' resetiamos la barra de progreso.
+        Dim input As Integer() = {1, 2, 3, 5, 7}
+
+        Dim output As New List(Of Integer)
+
+        Dim Progresodebarra As Integer = 0
+
+        ProgressBar1.Maximum = input.Count
+        output.Clear()
+        For i As Integer = 0 To input.Length - 1
+            For j As Integer = 0 To input.Length - 1
+                If input(i) <> input(j) AndAlso input(i) < 5 Then
+                    Dim result As Integer = (input(i) * 10 + input(j))
+                    output.Add(result)
+                End If
+            Next j
+            Progresodebarra += 1
+            ProgressBar1.Value = Progresodebarra
+        Next i
+
+        ListBox1.Items.AddRange(output.Cast(Of Object).Distinct().ToArray)
+        Progresodebarra = 0 'Ponemos en 0 como estaba al principio por si acaso volvemos a presionar.
+    End Sub
+
+    '/////////////////////////////////////////////////////////////////////////////////////////
+    'Fake Progress Bar . (se muestra un Progreso Falso)
+    '
+    'Se necesita :
+    '1 Timer .
+    '1 Progressbar.
+
+    Dim Fake_output As New List(Of Integer)
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ProgressBar1.Value = 0 ' resetiamos la barra de progreso.
+        ProgressBar1.Maximum = 100 ' Viene por defecto establecido en la barra de Progreso. pero igual lo declaramos.
+
+        Dim input As Integer() = {1, 2, 3, 5, 7}
+
+
+
+        For i As Integer = 0 To input.Length - 1
+            For j As Integer = 0 To input.Length - 1
+                If input(i) <> input(j) AndAlso input(i) < 5 Then
+                    Dim result As Integer = (input(i) * 10 + input(j))
+                    Fake_output.Add(result)
+                End If
+            Next j
+
+        Next i
+        Timer1.Enabled = True ' Iniciamos la barra de Progreso
+        Timer1.Interval = 100 ' Velocidad con la que carga la barra, con un numero mayor carga mas lento , y con un numero menor carga mas rapido .
+
+    End Sub
+
+
+    Dim Fake_Progresodebarra As Integer = 0
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Fake_Progresodebarra += 1
+        If Fake_Progresodebarra = ProgressBar1.Maximum Then
+            'aca  va lo que sucedera cuando la barra este llena. osea lo final.
+            ProgressBar1.Value = Fake_Progresodebarra
+            ListBox1.Items.AddRange(Fake_output.Cast(Of Object).Distinct().ToArray)
+            Fake_Progresodebarra = 0
+            Fake_output.Clear()
+            Timer1.Enabled = False ' detenemos la timer.
+        Else
+            ProgressBar1.Value = Fake_Progresodebarra
+        End If
+
+    End Sub
+
+End Class
